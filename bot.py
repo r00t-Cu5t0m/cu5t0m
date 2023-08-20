b7baf903150158cdc20e161ead954ed835d9f429
Cu5t0m <35927358+Sad-Anonym@users.noreply.github.com>
2023-08-20 16:43:07 +0300

    Add files via upload
    start coding

---
+++ bot.py
@@ -0,0 +1,19 @@
+import telebot
+from telebot import
+
+
+class build():
+        def buttons():
+
+        def menus():
+
+        def replys():
+
+        def pics():
+
+        build():
+
+        
+
+if __name__ == "__main__":
+        main()
